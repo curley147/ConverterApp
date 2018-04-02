3e2c2930221865d25bc9ce087080a840a437f8d9
MICHEAL CURLEY <G00340243@gmit.ie>
2018-04-02 16:50:33 +0100

    figuring out textbox controls

--- ConverterApp/MainPage.xaml.cs
+++ ConverterApp/MainPage.xaml.cs
@@ -47,11 +47,58 @@ namespace ConverterApp
                 rectangleFill.Color = Windows.UI.Colors.Red;
             }
         }*/
-        private void Text_Changing(TextBox sender, TextBoxTextChangingEventArgs args)
+        private void Cel_Changing(object sender, TextBoxTextChangingEventArgs args)
         {
+            fahrenheit.TextChanging -= Fah_Changing;
+            kelvin.TextChanging -= Kel_Changing;
+            if (celsius.Text == null)
+            {
+                celsius.Text = "0";
+            }
             double celDouble = Convert.ToDouble(celsius.Text);
-            fahrenheit.Text = Convert.ToString(celDouble * 10);
+            fahrenheit.Text = Convert.ToString((celDouble * 9 / 5.0) + 32);
+            kelvin.Text = Convert.ToString(celDouble + 273.15);
+
+            fahrenheit.TextChanging += Fah_Changing;
+            kelvin.TextChanging += Kel_Changing;
         }
-        
+
+        private void Fah_Changing(object sender, TextBoxTextChangingEventArgs args)
+        {
+            celsius.TextChanging -= Cel_Changing;
+            kelvin.TextChanging -= Kel_Changing;
+            if (fahrenheit.Text == null)
+            {
+                fahrenheit.Text = "0";
+            }
+            double fahDouble = Convert.ToDouble(fahrenheit.Text);
+            celsius.Text = Convert.ToString(((fahDouble - 32) * 5) / 9);
+            kelvin.Text = Convert.ToString(5 / 9 * (fahDouble - 32) + 273.15);
+
+            celsius.TextChanging += Cel_Changing;
+            kelvin.TextChanging += Kel_Changing;
+        }
+
+        private void Kel_Changing(object sender, TextBoxTextChangingEventArgs args)
+        {
+            celsius.TextChanging -= Cel_Changing;
+            fahrenheit.TextChanging -= Fah_Changing;
+            if (kelvin.Text == null)
+            {
+                kelvin.Text = "0";
+            }
+            double kelDouble = Convert.ToDouble(kelvin.Text);
+            celsius.Text = Convert.ToString(kelDouble - 273.15);
+            fahrenheit.Text = Convert.ToString(((kelDouble - 273) * 9 / 5) + 32);
+
+            celsius.TextChanging += Cel_Changing;
+            fahrenheit.TextChanging += Fah_Changing;
+        }
+
+        private void Temp_Changed(object sender, TextChangedEventArgs args)
+        {
+            
+        }
+
     }
 }
